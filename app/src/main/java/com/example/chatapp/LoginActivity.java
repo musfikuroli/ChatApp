0b0819cee1ea0338e7e7210c589da4988b29eff2
OLI <musfikuroli@gmail.com>
2021-06-16 21:56:07 +0600

    T24 - Phone Authentication Completed

--- app/src/main/java/com/example/chatapp/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/LoginActivity.java
@@ -52,6 +52,15 @@ public class LoginActivity extends AppCompatActivity {
                 AllowUserToLogin();
             }
         });
+
+        PhoneLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+
+                Intent phoneLoginIntent = new Intent(LoginActivity.this, PhoneLoginActivity.class);
+                startActivity(phoneLoginIntent);
+            }
+        });
     }
 
     private void AllowUserToLogin() {
